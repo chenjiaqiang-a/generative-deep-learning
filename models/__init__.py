6b85f1dc58d962f42f3166a71cef4f2fa3df0e34
chenjiaqiang-a <2504919775@qq.com>
2022-10-03 15:27:27 +0800

    compose

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,9 @@ from .GAN import GAN
 from .WGAN import WGAN
 from .WGANGP import WGANGP
 from .CycleGAN import CycleGAN
+from .RNNAttention import get_distinct, create_lookups, prepare_sequences, get_music_list, create_network, sample_with_temp
 
 __all__ = ["AutoEncoder", "VariationalAutoEncoder", "GAN", "WGAN",
-           "WGANGP", "CycleGAN"]
+           "WGANGP", "CycleGAN", "get_distinct", "create_lookups",
+           "prepare_sequences", "get_music_list", "create_network",
+           "sample_with_temp"]
